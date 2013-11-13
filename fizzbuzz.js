59f4c66ff21ba667d8420576e538332446fe59c2
Sota Sugiura <t11460ss@sfc.keio.ac.jp>
2013-11-13 14:43:21 +0900

    add js File

---
+++ fizzbuzz.js
@@ -0,0 +1,12 @@
+var num = 1;
+while(1){
+  if(num%3==0){
+    if(num%7==0){
+      document.write("FizzBuzz!");
+    } else {
+      document.write("Fizz");
+    }
+  } else if(num%7==0){
+    document.write("Buzz");
+  }
+}
