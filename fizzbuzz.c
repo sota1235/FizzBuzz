0b09972ced2fa8b43bfb7926803d5d6bb197e898
Sota Sugiura <t11460ss@sfc.keio.ac.jp>
2013-11-13 14:53:22 +0900

    fix fizzbuzz.c

--- fizzbuzz.c
+++ fizzbuzz.c
@@ -4,11 +4,11 @@ int main(void){
   int num = 1;
   while(1){
     if(num%3==0 && num%7==0){
-      printf("%d is FizzBuzz!",num);
+      printf("%d is FizzBuzz!\n",num);
     } else if(num%3==0){
-      printf("%d is Fizz",num);
+      printf("%d is Fizz\n",num);
     } else if(num%7==0){
-      printf("%d is Buzz",num);
+      printf("%d is Buzz\n",num);
     }
     num++;
   }
