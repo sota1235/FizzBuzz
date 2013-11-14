cc2fc2228bd584f664b1897de62fc459d2203c89
Sota Sugiura <t11460ss@sfc.keio.ac.jp>
2013-11-14 13:21:16 +0900

    fix fizzbuzz.js/less 3lines

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,14 +1,10 @@
 var num = 1;
-document.open();
 while(1){
   if(num%3==0){
     document.write(num%7==0 ? "FizzBuzz!<br />" : "Fizz<br />");
-  } else if(num%7==0){
-    document.write("Buzz<br />");
   } else {
-    document.write(num+"<br />");
+    document.write(num%7==0 ? "Buzz<br />" : num+"<br />");
   }
   num++;
   if(num==100) break;
 }
-document.close();
