e7d370c77d6385141399dfbbae7d2d70e60a8ff0
Sota Sugiura <t11460ss@sfc.keio.ac.jp>
2013-11-14 11:15:07 +0900

    rewrite fizzbuzz.php

--- fizzbuzz.php
+++ fizzbuzz.php
@@ -1,12 +1,13 @@
 <?php
 $i = 1;
-while(1){
+while(true){
   if($i % 3 == 0 && $i % 7 == 0){
-    echo $i++." is FizzBuzz!\n";
-  } else if($i % 3 == 0){
-    echo $i++." is Fizz\n";
-  } else if($i % 7 == 0){
-    echo $i++." is Buzz\n";
+    echo $i." is FizzBuzz!\n";
+  } else {
+  $str = ($i % 3) == 0 ? $i." is Fizz\n" : ( $str2 = ($i % 7) == 0 ? $i." is Buzz\n" : "" );
+  echo $str;
   }
+  $i++;
+  if($i == 100) break;
 }
 ?>
