334eac9506e1dcdec6ebfd1a672505890db0476c
Sota Sugiura <t11460ss@sfc.keio.ac.jp>
2013-11-14 14:47:41 +0900

    fix fizzbuzz.php

--- fizzbuzz.php
+++ fizzbuzz.php
@@ -1,11 +1,7 @@
 <?php
 $i = 1;
 while(true){
-  if($i % 3 == 0 && $i % 7 == 0){
-    echo "FizzBuzz!<br />";
-  } else {
-    echo $str = ($i % 3) == 0 ? "Fizz<br />" : ( $str2 = ($i % 7) == 0 ? "Buzz<br />" : $i."<br />" );
-  }
+	echo $str = ($i % 3 == 0 && $i % 7 == 0) ? "FizzBuzz!<br />" : ( ($i % 3) == 0 ? "Fizz<br />" : ( $str2 = ($i % 7) == 0 ? "Buzz<br />" : $i."<br />" ));
   if($i++ == 100) break;
 }
 ?>
