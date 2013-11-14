f9f2b16afafd4076617963ec6a15e1b9bf395ef1
Sota Sugiura <t11460ss@sfc.keio.ac.jp>
2013-11-14 14:09:12 +0900

    fix fizzbuzz.js

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # FizzBuzz
 
 * FizzBuzz programm written by some programming languges.
+
+## Rule
+
+* When number is multiple of 3 -> Fizz
+
+* When number is multiple of 7 -> Buzz
+
+* When number is multiple both of 3 and 7 -> FizzBuzz!
+
+* Aside from these -> output number
+
+* A scope is 1 to 100.
--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,10 +1,6 @@
 var num = 1;
 while(1){
-  if(num%3==0){
-    document.write(num%7==0 ? "FizzBuzz!<br />" : "Fizz<br />");
-  } else {
-    document.write(num%7==0 ? "Buzz<br />" : num+"<br />");
-  }
+  document.write(num%3==0 ? (num%7==0 ? "FizzBuzz!<br />" : "Fizz<br />") : (num%7==0 ? "Buzz<br />" : num+"<br />") );
   num++;
-  if(num==100) break;
+  if(num==101) break;
 }
