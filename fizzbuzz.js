372591a9689b658b9e1e01d295aa41107b202600
Sota Sugiura <t11460ss@sfc.keio.ac.jp>
2013-11-14 13:19:16 +0900

    fix fizzbuzz.js

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,18 +1,14 @@
 var num = 1;
+document.open();
 while(1){
   if(num%3==0){
-    /*
-    if(num%7==0){
-      document.write("FizzBuzz!");
-    } else {
-      document.write("Fizz");
-    }
-    */
-    document.write(num%7==0 ? "FizzBuzz!" : "Fizz");
+    document.write(num%7==0 ? "FizzBuzz!<br />" : "Fizz<br />");
   } else if(num%7==0){
-    document.write("Buzz");
+    document.write("Buzz<br />");
   } else {
-    document.write(num);
+    document.write(num+"<br />");
   }
   num++;
+  if(num==100) break;
 }
+document.close();
