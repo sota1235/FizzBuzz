fa8430439c86f22c32dfbefa622d785576505ca4
Sota Sugiura <t11460ss@sfc.keio.ac.jp>
2013-11-22 14:03:41 +0900

    fix fizzbuzz.c

--- fizzbuzz.c
+++ fizzbuzz.c
@@ -3,10 +3,8 @@
 int main(void){
   int num = 1;
   char str[12];
-  while(1){
+  for(num=1;num<=100;num++){
     sprintf(str,"%d",num);
     printf("%s\n",(num%3==0 && num%7==0) ? "FizzBuzz!" : (num%3==0 ? "Fizz" : (num%7==0 ? "Buzz" : str)));
-    num++;
-    if(num==101) break;
   }
 }
