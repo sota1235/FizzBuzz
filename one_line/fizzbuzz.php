61e2156276382b244cd3b413a8d75462780025f1
Sota Sugiura <t11460ss@sfc.keio.ac.jp>
2013-11-22 13:47:54 +0900

    one_line/fizzbuzz.php finish

--- one_line/fizzbuzz.php
+++ one_line/fizzbuzz.php
@@ -1,7 +1,1 @@
-<?php for($i=1;$i<=100;$i++) echo ($i%3==0 xor $i%7==0) ? ($i%3==0 ? "FizzBuzz<br />" : $i."<br />"): ($i%3==0 ? "Fizz<br />" : "Buzz<br />"); 
-if(2==2 xor 3==3){
-	echo "true";
-} else {
-	echo "false"
-}
-?>
+<?php for($i=1;$i<=100;$i++) echo ($i%3==0 xor $i%7==0) ? (i%3==0 ? "Fizz<br />" : "Buzz<br />") : ($i%3==0 ? "FizzBuzz<br />" : $i."<br />"); ?>
