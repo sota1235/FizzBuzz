cc02a93780f2211956baab0e1810109eed24060f
Sota Sugiura <t11460ss@sfc.keio.ac.jp>
2013-12-09 19:08:23 +0900

    fix inverse_fizzbuzz/fizzbuzz.py / とりあえずリスト追加

--- inverse_fizzbuzz/fizzbuzz.py
+++ inverse_fizzbuzz/fizzbuzz.py
@@ -1,1 +1,17 @@
+# coding: UTF-8
+# Copyright : sota1235
+# Date : 2013/12/09
+# HowToUse : py fizzbuzz.py str1 str2 str3...
+# fizz,buzz,fizzbuzzのリストを受け取り、
+# その並びが登場する始端と終端を返す
+
+import sys
+
+# fizz,buzz列
+list1 = {'Fizz','Fizz','Buzz','Fizz','Fizz','Buzz','Fizz','Fizz','FizzBuzz'}
+list2 = {'FizzBuzz','Fizz','Fizz','Buzz','Fizz','Fizz','Buzz','Fizz','Fizz'}
+
+argvs = sys.argv
+argc = len(argvs)
+
 
