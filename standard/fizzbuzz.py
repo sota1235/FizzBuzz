305a32cf9570aaaf956c286bf3a969382408cf4a
Sota Sugiura <t11460ss@sfc.keio.ac.jp>
2014-04-06 16:33:14 +0900

    fix standard/fizzbuzz.py / standard/.pyを少し改善

--- standard/fizzbuzz.py
+++ standard/fizzbuzz.py
@@ -1,10 +1,10 @@
-for i in range(100):
-    if (i+1)%3 == 0:
-        if (i+1)%7 == 0:
+for i in range(1,101):
+    if i%3 == 0:
+        if i%7 == 0:
             print "FizzBuzz"
         else:
             print "Fizz"
-    elif (i+1)%7 == 0:
+    elif i%7 == 0:
         print "Buzz"
     else:
-        print str(i+1)
+        print str(i)
