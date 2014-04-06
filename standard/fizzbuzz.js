4f65cb7fc1dec3275ca6330130162e6484a31b38
Sota Sugiura <t11460ss@sfc.keio.ac.jp>
2014-04-06 16:35:01 +0900

    fix /standard/fizzbuzz.js / node仕様に変更

--- standard/fizzbuzz.js
+++ standard/fizzbuzz.js
@@ -1,6 +1,3 @@
-var num = 1;
-while(1){
-  document.write(num%3==0 ? (num%7==0 ? "FizzBuzz!<br />" : "Fizz<br />") : (num%7==0 ? "Buzz<br />" : num+"<br />") );
-  num++;
-  if(num==101) break;
+for(var num=1;num<=100;num++){
+  console.log(num%3==0 ? (num%7==0 ? "FizzBuzz!" : "Fizz") : (num%7==0 ? "Buzz" : num) );
 }
